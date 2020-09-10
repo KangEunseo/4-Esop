863d69c584d21e288aa1eeaaae088e7696577ed6
KangEunseo <s2018w21@e-mirim.hs.kr>
2020-09-10 10:26:48 +0900

    templates/shop base, list, detail MEDIA

--- Esop/settings.py
+++ Esop/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'Esop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
